649325d1a8c1fdba8f5906656b620b5b45f948e5
nnukikordzaia <anuki.kordzaia@azry.com>
2021-12-26 00:01:11 +0400

    read product data from json.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,71 +1,80 @@
-import lombok.SneakyThrows;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import database.repository.ProductRepository;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException, SQLException {
+	public static void main(String[] args) {
+		HttpURLConnection connection;
+		BufferedReader reader;
+		String line;
+		StringBuffer responseContent = new StringBuffer();
 
-		String chromeDriverPath = "C:\\Users\\AzRy\\Desktop\\selenium\\web_driver\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+		try {
+			try {
+				URL url = new URL("https://api2.mymarket.ge/api/ka/products");
+				connection = (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("POST");
+				connection.setDoOutput(true);
 
-		String url = "https://www.mymarket.ge/ka/search/53/Laptop-kompiuterebi/?Attrs=82.86-138.146-3481.3488.3489.3490.3491-171.180&Brands=42&CatID=53&Page=1&PriceTo=1100";
+				Map<String, String> arguments = new HashMap<>();
+				arguments.put("Attrs", "82.87.86-138.146");
+				arguments.put("Brands", "42");
+				arguments.put("CatID", "53");
+				arguments.put("Limit", "26");
+				arguments.put("Page", "1");
+				arguments.put("PriceFrom", "800");
+				arguments.put("PriceTo", "1000");
+				arguments.put("SortID", "1");
+				StringJoiner sj = new StringJoiner("&");
+				for (Map.Entry<String, String> entry : arguments.entrySet())
+					sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+						+ URLEncoder.encode(entry.getValue(), "UTF-8"));
+				byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+				int length = out.length;
+				connection.setFixedLengthStreamingMode(length);
+				connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+				connection.connect();
+				try (OutputStream os = connection.getOutputStream()) {
+					os.write(out);
+				}
 
-		openTabs(url);
+				connection.setConnectTimeout(10000);
+				connection.setReadTimeout(10000);
 
-
-//
-//		ProductRepository repository = new ProductRepository();
-//		System.out.println(repository.getAllProduct());
-	}
-
-	public static void openTabs(String url) {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.get(url);
-		WebElement tabs = driver.findElement(By.className("search-content-cards"));
-		int tabsCount = tabs.findElements(By.tagName("a")).size();
-
-		for (int i = 0; i < tabsCount; i++) {
-			String openTabsAgain = Keys.chord(Keys.CONTROL, Keys.ENTER);
-			tabs.findElements(By.tagName("a")).get(i).sendKeys(openTabsAgain);
+				int status = connection.getResponseCode();
+				if (status > 299) {
+					reader = new BufferedReader((new InputStreamReader(connection.getErrorStream())));
+					while ((line = reader.readLine()) != null) {
+						responseContent.append(line);
+					}
+					reader.close();
+				} else {
+					reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+					while ((line = reader.readLine()) != null) {
+						responseContent.append(line);
+					}
+					reader.close();
+				}
+				System.out.println(responseContent);
+				JsonObject jsonObject = new JsonParser().parse(String.valueOf(responseContent)).getAsJsonObject();
+				System.out.println(jsonObject.getAsJsonObject("data").getAsJsonArray("Prs").get(0));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (JsonSyntaxException e) {
+			e.printStackTrace();
 		}
-		navigateInTabs(driver);
-	}
-
-	public static void navigateInTabs(WebDriver driver) {
-		ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
-		tabs.remove(0);
-		for (String s : tabs) {
-			driver.switchTo().window(s);
-			getInfoFromTab(driver);
-		}
-//
-	}
-
-	@SneakyThrows
-	public static void getInfoFromTab(WebDriver driver){
-		Thread.sleep(20000);
-		String price = driver.findElement(By.className("pr-price")).getText();
-		String brand = driver.findElement(By.cssSelector("#root > main > div > div.container.mt-32px.mb-40px > div > div > div.d-flex.spec-list.mt-32px.mt-md-72px > div > div > ul > li:nth-child(1) > p")).getText();
-		WebElement element = getElement(driver.findElement(By.className("opacity-50")));
-		if (element != null) {
-			System.out.println(price +" " +brand+ " " + element.getText());
-		}
-		System.out.println(price +" " +brand+ "" );
-	}
-
-	public static WebElement getElement(WebElement element) {
-		if(element.getText().equals("პროცესორის ტიპი")) {
-			return element;
-		}
-		return null;
 	}
 }
